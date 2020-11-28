5611a5c32704df7a6fe118d08e1afffa089428b0
Alex Brocklebank <alexbrocklebank@gmail.com>
2020-11-28 00:25:44 -0500

    Update generator.py
    Add Idea and Index route

--- generator.py
+++ generator.py
@@ -2,5 +2,10 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def initial():
-    return 'Hello World!'
+def index():
+    return 'Home Page'
+
+@app.route('/idea/<idea_id>')
+def show_idea(idea_id):
+    # Get Idea from db
+    return 'Idea'
