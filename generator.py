019868a396f19af4e391e7b2edcc9d6b21e87dc5
Alex Brocklebank <alexbrocklebank@gmail.com>
2020-11-28 02:35:16 -0500

    Add debug and title

--- generator.py
+++ generator.py
@@ -1,11 +1,14 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, url_for, render_template
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', title='Random Programming Project Generator')
 
 @app.route('/idea/<idea_id>')
 def show_idea(idea_id):
     # Get Idea from db
     return 'Idea'
+
+if __name__ == "__main__":
+    app.run(debug=True)
